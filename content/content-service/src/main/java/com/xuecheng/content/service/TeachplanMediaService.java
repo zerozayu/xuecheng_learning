df261dedf21d4ec3a3cd32725d67ce0aaec087f8
zerozayu <304307556@qq.com>
2023-12-12 23:48:56 +0800

    style: 删除无用依赖

--- content/content-service/src/main/java/com/xuecheng/content/service/TeachplanMediaService.java
+++ content/content-service/src/main/java/com/xuecheng/content/service/TeachplanMediaService.java
@@ -1,11 +1,7 @@
 package com.xuecheng.content.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
-import com.xuecheng.content.model.dto.SaveTeachplanDto;
-import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.TeachplanMedia;
-
-import java.util.List;
 
 /**
 * @author zhangyu
--- content/content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
+++ content/content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
@@ -2,11 +2,11 @@ package com.xuecheng.content.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xuecheng.content.mapper.TeachplanMapper;
 import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
 import com.xuecheng.content.service.TeachplanService;
-import com.xuecheng.content.mapper.TeachplanMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
