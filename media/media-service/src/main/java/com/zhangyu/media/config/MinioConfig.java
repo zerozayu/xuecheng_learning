f12ca408e9e7ba81b6f5cd97e8701e23b733f4ca
zerozayu <304307556@qq.com>
2024-01-15 23:36:49 +0800

    feat: 新增media-service

---
+++ media/media-service/src/main/java/com/zhangyu/media/config/MinioConfig.java
@@ -0,0 +1,32 @@
+package com.zhangyu.media.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * minio配置
+ *
+ * @author zhangyu
+ * @date 2024/1/15 23:31
+ */
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient = MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+        return minioClient;
+    }
+}
